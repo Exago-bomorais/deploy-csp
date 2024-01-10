8f47dd9c40985d502b2a6ac0ff4e6b52e6604404
Bruno Oliveira de Morais <exago.bomorais@universal.ad>
2024-01-10 10:20:02 -0300

    second commit

--- middleware.ts
+++ middleware.ts
@@ -42,13 +42,7 @@ export function middleware(request: any) {
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
+    '/',
     {
       source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
       missing: [
